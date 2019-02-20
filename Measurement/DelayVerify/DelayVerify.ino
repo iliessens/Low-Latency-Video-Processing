91ebca7718c565fe0763fe59361fc23bac59e0da
Imre Liessens <imre.liessens@telenet.be>
2019-02-20 15:48:19 +0100

    Add flag for library

--- Measurement/DelayVerify/DelayVerify.ino
+++ Measurement/DelayVerify/DelayVerify.ino
@@ -1,8 +1,14 @@
 // library for analog comparator
 // Download from http://www.leonardomiliani.com/en/2012/analogcomp-una-libreria-per-gestire-il-comparatore-analogico/ and place in libraries folder
 #include "analogComp.h"
+
+// Should fast library be used?
+#define USEFAST 0
+
+#if USEFAST
 // Faster I/O library from https://github.com/NicksonYap/digitalWriteFast
 #include <digitalWriteFast.h>
+#endif
 
 int state = 0;
 const int outPin = 5;
@@ -12,7 +18,11 @@ void setup() {
   pinMode(refPin, OUTPUT);   // sets the pin as output
   analogWrite(refPin, 128);
 
+#if USEFAST
   pinModeFast(outPin, OUTPUT);
+#else
+  pinMode(outPin, OUTPUT);
+#endif
   // setup comparator
   analogComparator.setOn(AIN0, AIN1); // sensor: D6, reference: D7 on Arduino Uno
 
@@ -21,10 +31,18 @@ void setup() {
 
 void edgeDetected() {
   if (state == 1) { // zo geschreven omdat bibliotheek constanten nodig heeft
+#if USEFAST
     digitalWriteFast(outPin, LOW);
+#else
+    digitalWrite(outPin, LOW);
+#endif
   }
   else {
+#if USEFAST
     digitalWriteFast(outPin, HIGH);
+#else
+    digitalWrite(outPin, HIGH);
+#endif
   }
   state = ! state;
 }
