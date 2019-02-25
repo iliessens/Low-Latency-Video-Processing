c69416d0d90e63746eb7303dda7b7daf465b2912
Imre Liessens <imre.liessens@telenet.be>
2019-02-25 13:22:36 +0100

    Refactor and add device selection

--- BMD-software-overlay/DeviceList.cpp
+++ BMD-software-overlay/DeviceList.cpp
@@ -5,58 +5,84 @@
 #include <comutil.h>
 #include "DeckLinkAPI_h.h"
 
-int	_tmain (int argc, _TCHAR* argv[])
-{
-	IDeckLinkIterator*			deckLinkIterator;
-	IDeckLinkAPIInformation*	deckLinkAPIInformation;
-	IDeckLink*					deckLink;
-	int							numDevices = 0;
-	HRESULT						result;
-	
+#include "settings.h"
+
+IDeckLinkIterator* initCom() {
+	IDeckLinkIterator* deckLinkIterator;
+
 	// Initialize COM on this thread
-	result = CoInitialize(NULL);
+	HRESULT result = CoInitialize(NULL);
 	if (FAILED(result))
 	{
 		fprintf(stderr, "Initialization of COM failed - result = %08x.\n", result);
-		return 1;
+		return NULL;
 	}
-	
+
 	// Create an IDeckLinkIterator object to enumerate all DeckLink cards in the system
 	result = CoCreateInstance(CLSID_CDeckLinkIterator, NULL, CLSCTX_ALL, IID_IDeckLinkIterator, (void**)&deckLinkIterator);
 	if (FAILED(result))
 	{
 		fprintf(stderr, "A DeckLink iterator could not be created.  The DeckLink drivers may not be installed.\n");
-		return 1;
+		return NULL;
 	}
 	
-	
+	return deckLinkIterator;
+}
+
+IDeckLink* findDeckLink(IDeckLinkIterator* deckLinkIterator) {
+	IDeckLink*		selectedDecklink = NULL;
+	IDeckLink*		deckLink;
+	int			numDevices = 0;
+	HRESULT		result;
+
 	// Enumerate all cards in this system
 	while (deckLinkIterator->Next(&deckLink) == S_OK)
 	{
 		BSTR		deviceNameBSTR = NULL;
-		
+
 		// Increment the total number of DeckLink cards found
 		numDevices++;
-		
+
+		// save pointer to selected device
+		if (numDevices == BMD_DEVICE) {
+			selectedDecklink = deckLink;
+			printf("-> ");
+		}
+
 		// *** Print the model name of the DeckLink card
 		result = deckLink->GetModelName(&deviceNameBSTR);
 		if (result == S_OK)
 		{
 			_bstr_t		deviceName(deviceNameBSTR, false);
-			
+
 			printf("(%d) %s \n", numDevices, (char*)deviceName);
 		}
-		
+
 		// Release the IDeckLink instance when we've finished with it to prevent leaks
 		deckLink->Release();
 	}
-	
+
 	deckLinkIterator->Release();
-	
+
 	// If no DeckLink cards were found in the system, inform the user
 	if (numDevices == 0)
 		printf("No Blackmagic Design devices were found.\n");
+
+	return selectedDecklink;
+}
+
+int	_tmain (int argc, _TCHAR* argv[])
+{
+	HRESULT						result;
+	
+	IDeckLinkIterator* deckLinkIterator = initCom();
+	if (deckLinkIterator == NULL) return 1;
+
+	IDeckLink*		deckLink = findDeckLink(deckLinkIterator);
+
 	printf("\n");
+	if (deckLink == NULL)
+		printf("The selected device was not found!\n");
 	
 	// Uninitalize COM on this thread
 	CoUninitialize();
---
+++ BMD-software-overlay/settings.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#define BMD_DEVICE 1
