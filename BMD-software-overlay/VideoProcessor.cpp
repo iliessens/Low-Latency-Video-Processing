eb27e9085337aa961ba425d549515fb069db963d
Imre Liessens <imre.liessens@telenet.be>
2019-03-01 11:43:10 +0100

    Unpack pixel data

--- BMD-software-overlay/VideoProcessor.cpp
+++ BMD-software-overlay/VideoProcessor.cpp
@@ -1,12 +1,24 @@
 #include "VideoProcessor.h"
+#include "settings.h"
+#include <assert.h>
 
 VideoProcessor::VideoProcessor() {
-
+	assert(PIXEL_MODE == BMDPixelFormat::bmdFormat8BitBGRA); // only this supported for now
 }
 
 void VideoProcessor::publishFrame(IDeckLinkVideoFrame * frame)
 {
-	// passthrough
+	long height = frame->GetHeight();
+	long width = frame->GetWidth();
+	long dataLength = width * height;
+
+	int* frameDataPointer;
+	frame->GetBytes((void**) &frameDataPointer);
+
+	for (int i = 0; i < dataLength; i = i + 2) {
+		*(frameDataPointer + i) = 0; // set every second pixel black
+	}
+
 	int fail = output->showFrame(frame);
 	if (fail) printf("Error outputting frame\n");
 }
