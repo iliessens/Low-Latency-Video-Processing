00afa06a1e5d9f1d9d6636e071e423b2a1d937de
Imre Liessens <imre.liessens@telenet.be>
2019-03-06 14:55:09 +0100

    First SSE experiment

--- BMD-software-overlay/VideoProcessor.cpp
+++ BMD-software-overlay/VideoProcessor.cpp
@@ -2,8 +2,46 @@
 #include "settings.h"
 #include <assert.h>
 #include <thread>         // std::thread
+#include <intrin.h>
 
 volatile uint8_t * currentFrame;
+
+// Source: http://www.alfredklomp.com/programming/sse-intrinsics/
+static inline __m128i
+_mm_div255_epu16(__m128i x)
+{
+	// Divide 8 16-bit uints by 255:
+	// x := ((x + 1) + (x >> 8)) >> 8:
+	return _mm_srli_epi16(_mm_adds_epu16(
+		_mm_adds_epu16(x, _mm_set1_epi16(1)),
+		_mm_srli_epi16(x, 8)), 8);
+}
+
+// Probably too big to want to inline:
+static __m128i
+_mm_scale_epu8(__m128i x, __m128i y)
+{
+	// Returns an "alpha blend" of x scaled by y/255;
+	//   x := x * (y / 255)
+	// Reorder: x := (x * y) / 255
+
+	// Unpack x and y into 16-bit uints:
+	__m128i xlo = _mm_unpacklo_epi8(x, _mm_setzero_si128());
+	__m128i ylo = _mm_unpacklo_epi8(y, _mm_setzero_si128());
+	__m128i xhi = _mm_unpackhi_epi8(x, _mm_setzero_si128());
+	__m128i yhi = _mm_unpackhi_epi8(y, _mm_setzero_si128());
+
+	// Multiply x with y, keeping the low 16 bits:
+	xlo = _mm_mullo_epi16(xlo, ylo);
+	xhi = _mm_mullo_epi16(xhi, yhi);
+
+	// Divide by 255:
+	xlo = _mm_div255_epu16(xlo);
+	xhi = _mm_div255_epu16(xhi);
+
+	// Repack the 16-bit uints to clamped 8-bit values:
+	return _mm_packus_epi16(xlo, xhi);
+}
 
 void processBlock(uint8_t* overlayPtr, long startOffset, int numBytes) {
 
@@ -11,19 +49,21 @@ void processBlock(uint8_t* overlayPtr, long startOffset, int numBytes) {
 		frameDataPointer = frameDataPointer + startOffset;
 		overlayPtr = overlayPtr + startOffset;
 
-		for (int j = 0; j < numBytes; j = j + 4) {
+		for (int j = 0; j < numBytes; j = j + 16) {
 			uint8_t* pixelPtr = frameDataPointer + j ;
 			uint8_t* overlayPixelPtr = overlayPtr + j;
 
-			uint8_t w = 255 - *(overlayPixelPtr + 3); // 1 - alpha
+			for (int i = 0; i <= 4; ++i) { // preprocess 4 pixels
 
-			for (int k = 0; k < IMAGE_CHANNELS - 1; ++k) {
-				uint16_t temp = *(pixelPtr + k) * w;
-				*(pixelPtr + k) = (temp >> 8) + *(overlayPixelPtr + k); // divide by 255 and add
+				uint8_t w = 255 - *(overlayPixelPtr + 3 + i * 4); // 1 - alpha
+
+				for (int k = 0; k < IMAGE_CHANNELS - 1; ++k) {
+					uint16_t temp = *(pixelPtr + k + i*4) * w;
+					*(pixelPtr + k + i*4) = (temp >> 8); // divide by 255 and add
+				}
 			}
-			//*(pixelPtr) = 0xFF;
-			//*(pixelPtr + 1) = 0;
-			//*(pixelPtr + 2) = 0;
+
+			_mm_store_si128((__m128i*) pixelPtr, _mm_add_epi8(*(__m128i*)pixelPtr, *(__m128i*)  overlayPixelPtr));
 		}
 }
 
