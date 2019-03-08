2c7fb69e1ef5a9bc8ba87e35c96eb92d10dde4bd
Imre Liessens <imre.liessens@telenet.be>
2019-03-08 11:23:31 +0100

    Changed all operation to AVX-512

--- BMD-software-overlay/VideoProcessor.cpp
+++ BMD-software-overlay/VideoProcessor.cpp
@@ -7,54 +7,56 @@
 volatile uint8_t * currentFrame;
 
 // Source: http://www.alfredklomp.com/programming/sse-intrinsics/
-static inline __m128i
-_mm_div255_epu16(__m128i x)
+static inline __m512i
+_mm512_div255_epu16(__m512i x)
 {
 	// Divide 8 16-bit uints by 255:
 	// x := ((x + 1) + (x >> 8)) >> 8:
-	return _mm_srli_epi16(_mm_adds_epu16(
-		_mm_adds_epu16(x, _mm_set1_epi16(1)),
-		_mm_srli_epi16(x, 8)), 8);
+	return _mm512_srli_epi16(_mm512_adds_epu16(
+		_mm512_adds_epu16(x, _mm512_set1_epi16(1)),
+		_mm512_srli_epi16(x, 8)), 8);
 }
 
 // Probably too big to want to inline:
-static __m128i
-_mm_scale_epu8(__m128i x, __m128i y)
+static __m512i
+_mm512_scale_epu8(__m512i x, __m512i y)
 {
 	// Returns an "alpha blend" of x scaled by y/255;
 	//   x := x * (y / 255)
 	// Reorder: x := (x * y) / 255
 
 	// Unpack x and y into 16-bit uints:
-	__m128i xlo = _mm_unpacklo_epi8(x, _mm_setzero_si128());
-	__m128i ylo = _mm_unpacklo_epi8(y, _mm_setzero_si128());
-	__m128i xhi = _mm_unpackhi_epi8(x, _mm_setzero_si128());
-	__m128i yhi = _mm_unpackhi_epi8(y, _mm_setzero_si128());
+	__m512i xlo = _mm512_unpacklo_epi8(x, _mm512_setzero_si512());
+	__m512i ylo = _mm512_unpacklo_epi8(y, _mm512_setzero_si512());
+	__m512i xhi = _mm512_unpackhi_epi8(x, _mm512_setzero_si512());
+	__m512i yhi = _mm512_unpackhi_epi8(y, _mm512_setzero_si512());
 
 	// Multiply x with y, keeping the low 16 bits:
-	xlo = _mm_mullo_epi16(xlo, ylo);
-	xhi = _mm_mullo_epi16(xhi, yhi);
+	xlo = _mm512_mullo_epi16(xlo, ylo);
+	xhi = _mm512_mullo_epi16(xhi, yhi);
 
-	// Divide by 255:
-	xlo = _mm_div255_epu16(xlo);
-	xhi = _mm_div255_epu16(xhi);
+	// Divide by 255: (custom functie, zie boven)
+	xlo = _mm512_div255_epu16(xlo);
+	xhi = _mm512_div255_epu16(xhi);
 
 	// Repack the 16-bit uints to clamped 8-bit values:
-	return _mm_packus_epi16(xlo, xhi);
+	return _mm512_packus_epi16(xlo, xhi);
 }
 
-// load 4 alpha values into 128 bit register
- inline __m128i createAlphaVector(uint8_t* overlay) {
-	const uint8_t pix1 = 255 - *(overlay + 3);
-	const uint8_t pix2 = 255 - *(overlay + 7);
-	const uint8_t pix3 = 255 - *(overlay + 11);
-	const uint8_t pix4 = 255 - *(overlay + 15);
-	const __m128i scalar = _mm_setr_epi8(pix1, pix1, pix1, pix1,
-		pix2, pix2, pix2, pix2,
-		pix3, pix3, pix3, pix3,
-		pix4, pix4, pix4, pix4);
+// load 4 alpha values into 512 bit register
+ inline __m512i createAlphaVector(uint8_t* overlay) {
+	 uint8_t* scalars = (uint8_t*) malloc(64); //64 bytes = 512 bits
 
-	return scalar;
+	 for (int i = 0; i < 16; ++i) {
+		 uint8_t pix = 255 - *(overlay + 4*i + 3);
+		 for (int j = 0; j < 4; ++j) {
+			 *(scalars + j +  4*i) = pix;
+		 }
+	 }
+
+	 __m512i result = _mm512_load_si512(scalars);
+	free(scalars); // release temporary buffer
+	return result;
 }
 
 
@@ -64,26 +66,18 @@ void processBlock(uint8_t* overlayPtr, long startOffset, int numBytes) {
 		frameDataPointer = frameDataPointer + startOffset;
 		overlayPtr = overlayPtr + startOffset;
 
-		for (int j = 0; j < numBytes; j = j + 16) {
+		for (int j = 0; j < numBytes; j = j + 64) {
 			uint8_t* pixelPtr = frameDataPointer + j ;
 			uint8_t* overlayPixelPtr = overlayPtr + j;
 			
 			// fil vector with alpha values to make one-by-one multiplication possible
-			__m128i alpha = createAlphaVector(overlayPixelPtr);
+			__m512i alpha = createAlphaVector(overlayPixelPtr);
 
 			//scale incoming image with alpha value
-			__m128i multiplied = _mm_scale_epu8(*(__m128i*) pixelPtr, alpha);
+			__m512i multiplied = _mm512_scale_epu8(*(__m512i*) pixelPtr, alpha);
 
-			// write result to image memory
-			_mm_store_si128((__m128i*) pixelPtr, multiplied);
-
-		}
-
-		//Second loop for instructions that process multiple pixels
-		for (int j = 0; j < numBytes; j = j + 64) {
-			uint8_t* pixelPtr = frameDataPointer + j;
-			uint8_t* overlayPixelPtr = overlayPtr + j;
-			_mm512_store_si512((__m512i*)pixelPtr, _mm512_add_epi8(*(__m512i*)pixelPtr, *(__m512i*)overlayPixelPtr));
+			//add overlay and image
+			_mm512_store_si512((__m512i*)pixelPtr, _mm512_add_epi8(multiplied, *(__m512i*)overlayPixelPtr));
 		}
 }
 
