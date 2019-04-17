329628ec40ba25747ab7999c5bc0df4f7f309b3b
Imre Liessens <imre.liessens@telenet.be>
2019-04-17 19:31:49 +0200

    OpenCL build OK

--- IP-ffmpeg-scripts/ffmpeg_configure.sh
+++ IP-ffmpeg-scripts/ffmpeg_configure.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/sh
+#Print commands
 set -x
+#Fail on errors
+set -e
 cd ../../ffmpeg
+
 PKG_CONFIG_PATH="nv-codec-headers/"
-./configure --enable-nvenc --enable-nvdec --enable-opengl --enable-sdl --disable-shared --enable-static \
- --enable-decklink --extra-cflags=-I"decklink-headers/" --extra-ldflags=-L"decklink-headers/"
+./configure --enable-nvenc --enable-nvdec --enable-opengl --enable-opencl --enable-sdl --disable-shared --enable-static \
+--extra-cflags=-I"opencl/include/" --extra-ldflags=-L"opencl/lib/"
  make -j8
