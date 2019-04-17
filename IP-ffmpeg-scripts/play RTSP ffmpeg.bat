3a4830da63836450f5130d7a47554bc409c476d5
Imre Liessens <imre.liessens@telenet.be>
2019-04-17 10:27:38 +0200

    Optimize scripts and add cuvid example

--- IP-ffmpeg-scripts/play RTSP ffmpeg.bat
+++ IP-ffmpeg-scripts/play RTSP ffmpeg.bat
@@ -1,5 +1,5 @@
 call setpath
 ffmpeg -hwaccel nvdec -threads 2 ^
+-flags low_delay -fflags nobuffer ^
 -i rtsp://192.168.100.150:8557/h264 ^
--fflags nobuffer -flags low_delay ^
 -pix_fmt bgra -f sdl "Video"
