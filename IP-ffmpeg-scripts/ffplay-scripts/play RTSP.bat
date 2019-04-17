e02ff88ef7d687fe79f3252ee3610f2c8da4b3a2
Imre Liessens <imre.liessens@telenet.be>
2019-04-17 18:32:15 +0200

    Use own ffplay build

--- IP-ffmpeg-scripts/ffplay-scripts/play RTSP.bat
+++ IP-ffmpeg-scripts/ffplay-scripts/play RTSP.bat
@@ -1,4 +1,4 @@
-call setpath
+call ../setpath
 ffplay -fflags nobuffer -flags low_delay -framedrop -strict experimental rtsp://192.168.100.150:8557/h264 -sync ext
 REM rtsp://192.168.100.150:8555/mjpeg
 pause
