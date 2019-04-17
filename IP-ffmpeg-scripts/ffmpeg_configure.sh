87522e1b07cfbfa8ea0b239a5b7865825921f1ab
Imre Liessens <imre.liessens@telenet.be>
2019-04-17 18:32:33 +0200

    Remove OpenCL build

--- IP-ffmpeg-scripts/ffmpeg_configure.sh
+++ IP-ffmpeg-scripts/ffmpeg_configure.sh
@@ -2,6 +2,6 @@
 set -x
 cd ../../ffmpeg
 PKG_CONFIG_PATH="nv-codec-headers/"
-./configure --enable-nvenc --enable-nvdec --enable-opengl --enable-opencl --enable-sdl --disable-shared --enable-static \
+./configure --enable-nvenc --enable-nvdec --enable-opengl --enable-sdl --disable-shared --enable-static \
  --enable-decklink --extra-cflags=-I"decklink-headers/" --extra-ldflags=-L"decklink-headers/"
  make -j8
