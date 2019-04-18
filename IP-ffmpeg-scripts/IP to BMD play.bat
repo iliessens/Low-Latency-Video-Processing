afbe332736a155c2bad686f9841a257ccc322f23
Imre Liessens <imre.liessens@telenet.be>
2019-04-18 10:48:44 +0200

    Improve script. Still not good

--- IP-ffmpeg-scripts/IP to BMD play.bat
+++ IP-ffmpeg-scripts/IP to BMD play.bat
@@ -1,7 +1,9 @@
 REM way too much latency
-REM low_latency option causes glitches on screen
 call setpath
 ffmpeg -hwaccel nvdec -threads 2 ^
+-max_delay 0 ^
+-flags low_delay -fflags nobuffer ^
 -i rtsp://192.168.100.150:8557/h264 ^
+-flags low_delay ^
 -f decklink -pix_fmt uyvy422 "Intensity Pro 4K"
 pause
