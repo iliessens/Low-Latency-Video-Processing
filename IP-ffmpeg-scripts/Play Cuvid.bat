0fc2007e79ffad4aa75a8db2e38585120159e0c8
Imre Liessens <imre.liessens@telenet.be>
2019-04-18 12:21:55 +0200

    Small script improvements

--- IP-ffmpeg-scripts/Play Cuvid.bat
+++ IP-ffmpeg-scripts/Play Cuvid.bat
@@ -1,6 +1,7 @@
 call setpath
 ffmpeg -hwaccel cuvid -threads 2 ^
 -c:v h264_cuvid ^
+-max_delay 0 ^
 -i rtsp://192.168.100.150:8557/h264 ^
 -filter_complex "hwdownload,format=nv12" ^
 -fflags nobuffer -flags low_delay ^
