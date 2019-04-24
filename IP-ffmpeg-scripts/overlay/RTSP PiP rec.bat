b43277a43425494e96d700663a6cc78f1feffde0
Imre Liessens <imre.liessens@telenet.be>
2019-04-24 14:13:40 +0200

    Improve PiP

--- IP-ffmpeg-scripts/overlay/RTSP PiP rec.bat
+++ IP-ffmpeg-scripts/overlay/RTSP PiP rec.bat
@@ -1,7 +1,7 @@
 REM use a previous recording using the streamdump script to simulate a second camera
 REM Recorded video will freeze on last frame
 call ../setpath
-ffmpeg -hwaccel nvdec -threads 4 ^
+ffmpeg -threads 4 ^
 -max_delay 0 -fflags nobuffer -flags low_delay ^
 -i rtsp://192.168.100.150:8557/h264 ^
 -i ../../../streamdump.ts ^
