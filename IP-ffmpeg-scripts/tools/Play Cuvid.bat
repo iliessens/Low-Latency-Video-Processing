1de9125d8a3657492bbff3fb36d9e9dd6dd2eeec
Imre Liessens <imre.liessens@telenet.be>
2019-05-03 10:02:47 +0200

    Small updates

--- IP-ffmpeg-scripts/tools/Play Cuvid.bat
+++ IP-ffmpeg-scripts/tools/Play Cuvid.bat
@@ -1,7 +1,8 @@
 call ../setpath
 ffmpeg -hwaccel cuvid -threads 2 ^
 -c:v h264_cuvid ^
--max_delay 0 ^
+-buffer_size 20M ^
+-max_delay 0 -fflags nobuffer -flags low_delay ^
 -i rtsp://192.168.100.150:8557/h264 ^
 -filter_complex "hwdownload,format=nv12" ^
 -fflags nobuffer -flags low_delay ^
--- IP-ffmpeg-scripts/tools/play RTSP ffmpeg.bat
+++ IP-ffmpeg-scripts/tools/play RTSP ffmpeg.bat
@@ -1,6 +1,7 @@
 call ../setpath
-ffmpeg -hwaccel nvdec -threads 2 ^
+ffmpeg -hwaccel nvdec -threads 8 ^
 -max_delay 0 ^
 -flags low_delay -fflags nobuffer ^
 -i rtsp://192.168.100.150:8557/h264 ^
+-window_fullscreen 1 ^
 -pix_fmt bgra -f sdl "Video"
