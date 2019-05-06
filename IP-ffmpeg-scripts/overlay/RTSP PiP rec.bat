50da62736122993bc47de178a23f015257e3e93a
Imre Liessens <imre.liessens@telenet.be>
2019-05-06 09:29:41 +0200

    Consistency improvements

--- IP-ffmpeg-scripts/overlay/RTSP PiP rec.bat
+++ IP-ffmpeg-scripts/overlay/RTSP PiP rec.bat
@@ -6,5 +6,6 @@ ffmpeg -threads 4 -hwaccel none ^
 -i rtsp://192.168.100.150:8557/h264 ^
 -i ../../../streamdump.ts ^
 -filter_complex "[1:v]scale=iw/4:ih/4, drawbox=color=red@0.5, pad=w=iw*4:h=ih*4:x=ow-iw:y=oh-ih:color=0x00000000[pip]; [0:v][pip]overlay=0:0:enable=1" ^
+-window_fullscreen 1 ^
 -fflags nobuffer -flags low_delay -pix_fmt bgra -f sdl "Video"
 timeout 10
--- IP-ffmpeg-scripts/overlay/overlay RTSP.bat
+++ IP-ffmpeg-scripts/overlay/overlay RTSP.bat
@@ -1,8 +1,8 @@
 call ../setpath
 ffmpeg -hwaccel none -threads 2 ^
--max_delay 0 ^
+-max_delay 0 -fflags nobuffer -flags low_delay ^
 -i rtsp://192.168.100.150:8557/h264 -i 1080_Test_small.tga ^
--fflags nobuffer -flags low_delay -filter_complex "[0:v][1:v]overlay=0:0:enable=1" ^
+-filter_complex "[0:v][1:v]overlay=0:0:enable=1" ^
 -window_fullscreen 1 ^
 -pix_fmt bgra -f sdl "Video"
 timeout 10
