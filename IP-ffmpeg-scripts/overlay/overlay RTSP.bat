d7f31fa61d00c124c9b0573af701207c2643bf3e
Imre Liessens <imre.liessens@telenet.be>
2019-05-08 11:07:12 +0200

    Consistency

--- IP-ffmpeg-scripts/overlay/overlay RTSP.bat
+++ IP-ffmpeg-scripts/overlay/overlay RTSP.bat
@@ -1,5 +1,5 @@
 call ../setpath
-ffmpeg -hwaccel none -threads 2 ^
+ffmpeg -hwaccel none -threads 8 ^
 -max_delay 0 -fflags nobuffer -flags low_delay ^
 -i rtsp://192.168.100.150:8557/h264 -i 1080_Test_small.tga ^
 -filter_complex "[0:v][1:v]overlay=0:0:enable=1" ^
--- IP-ffmpeg-scripts/tools/play RTSP ffmpeg.bat
+++ IP-ffmpeg-scripts/tools/play RTSP ffmpeg.bat
@@ -1,5 +1,5 @@
 call ../setpath
-ffmpeg -hwaccel nvdec -threads 8 ^
+ffmpeg -hwaccel none -threads 8 ^
 -max_delay 0 ^
 -flags low_delay -fflags nobuffer ^
 -i rtsp://192.168.100.150:8557/h264 ^
