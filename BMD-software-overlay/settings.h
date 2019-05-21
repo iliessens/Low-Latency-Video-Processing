3d3f994d8e9148e031ed2eb64a821b54614adb27
Imre Liessens <imre.liessens@telenet.be>
2019-05-21 15:01:09 +0200

    Auto-load correct images

--- BMD-software-overlay/settings.h
+++ BMD-software-overlay/settings.h
@@ -14,8 +14,14 @@
 #define EN_CHRONO 0
 
 //image settings
+#if WIDTH == 1920
 #define NO_SIGNAL_IMG "nosignal_1080.tga"
 #define IMAGE_NAME "../../1080_Test_small.tga"
+#elif WIDTH == 1280
+#define NO_SIGNAL_IMG "nosignal.tga"
+#define IMAGE_NAME "../../720_Test_small.tga"
+#endif
+
 #define IMAGE_CHANNELS 4
 
 #define NUM_THREADS 2
